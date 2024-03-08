8f659ce1e40931810294d914d0d7c0228d9a8653
Preslav977 <p.cvetanow@gmail.com>
2024-03-08 15:20:13 +0200

    Added additional logic to render the index page on default and page 404 when the urls is different

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const http = require("http");
 const fs = require("node:fs");
 const url = require("node:url");
 
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  return data;
+});
+
 http
   .createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
+    const q = url.parse(req.url, true);
+    let filename = "";
+
+    if (q.pathname === "/") {
+      filename = "." + "/index.html";
+    } else {
+      filename = "." + q.pathname;
+    }
+
     fs.readFile(filename, function (err, data) {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
+        res.write(page404);
+        return res.end();
       }
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
